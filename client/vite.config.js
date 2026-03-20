30333d939962d9de58738c77789ae9c1d5cd4896
saravana47kumar <155990476+saravana47kumar@users.noreply.github.com>
2026-03-20 19:18:23 +0530

    Update vite.config.js

--- client/vite.config.js
+++ client/vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
     port: 5173,
     proxy: {
       '/api': {
-        target: 'http://localhost:5000',
+        target: 'https://e-ration-g7cb.onrender.com',
         changeOrigin: true,
       }
     }
